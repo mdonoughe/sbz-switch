9d7949b4cdb97345813fe6b62bc52683c47f83a1
Matthew Donoughe <mdonoughe@gmail.com>
2019-05-12 00:36:16 -0400

    fix deadlock in event monitor

--- src/com/event.rs
+++ src/com/event.rs
@@ -1,27 +1,28 @@
 use futures::executor::{self, Notify, NotifyHandle, Spawn};
 use futures::{Async, Stream};
 
-use std::mem;
-use std::ptr;
+use std::collections::{BTreeMap, VecDeque};
 use std::sync::{Arc, Mutex};
+use std::{mem, ptr};
 
 use winapi::um::combaseapi::{CoWaitForMultipleObjects, CWMO_DISPATCH_CALLS};
 use winapi::um::handleapi::CloseHandle;
-use winapi::um::synchapi::{CreateEventW, SetEvent};
+use winapi::um::synchapi::{CreateEventW, ResetEvent, SetEvent};
 use winapi::um::winbase::INFINITE;
+use winapi::um::winnt::HANDLE;
 
 use crate::hresult::check;
 
 struct ComUnparkState {
-    handles: Vec<usize>,
-    refs: Vec<usize>,
+    handles: BTreeMap<usize, usize>,
+    ready_event: HANDLE,
+    ready: VecDeque<usize>,
+    next_id: usize,
 }
 
 impl Drop for ComUnparkState {
     fn drop(&mut self) {
-        for handle in self.handles.iter() {
-            unsafe { CloseHandle(*handle as *mut _) };
-        }
+        unsafe { CloseHandle(self.ready_event as *mut _) };
     }
 }
 
@@ -35,70 +36,85 @@ unsafe impl Sync for ComUnpark {}
 
 impl ComUnpark {
     pub fn new() -> Self {
+        let ready_event = unsafe { CreateEventW(ptr::null_mut(), 1, 0, ptr::null_mut()) };
         Self {
             state: Arc::new(Mutex::new(ComUnparkState {
-                handles: Vec::new(),
-                refs: Vec::new(),
+                handles: BTreeMap::new(),
+                ready_event,
+                ready: VecDeque::new(),
+                next_id: 0,
             })),
         }
     }
 
     pub fn allocate_id(&self) -> usize {
         let mut state = self.state.lock().unwrap();
-        let handle = unsafe { CreateEventW(ptr::null_mut(), 0, 0, ptr::null_mut()) as usize };
-        let pos = state.handles.binary_search(&handle).err().unwrap();
-        state.handles.insert(pos, handle);
-        state.refs.insert(pos, 1);
-        handle
+        let id = state.next_id;
+        state.next_id += 1;
+        state.handles.insert(id, 1);
+        id
     }
 
     pub fn park(&self) -> usize {
-        let state = self.state.lock().unwrap();
-        let which = unsafe {
-            let mut which = mem::uninitialized();
-            check(CoWaitForMultipleObjects(
-                CWMO_DISPATCH_CALLS,
-                INFINITE,
-                state.handles.len() as u32,
-                &state.handles[0] as *const usize as *const _,
-                &mut which as *mut _,
-            ))
-            .expect("failed to wait for unpark");
-            which
+        let (ready_event, ready) = {
+            let mut state = self.state.lock().unwrap();
+            (state.ready_event, state.ready.pop_front())
         };
-        state.handles[which as usize]
+        if let Some(ready) = ready {
+            return ready;
+        }
+        loop {
+            unsafe {
+                let mut which = mem::uninitialized();
+                check(CoWaitForMultipleObjects(
+                    CWMO_DISPATCH_CALLS,
+                    INFINITE,
+                    1,
+                    &ready_event,
+                    &mut which as *mut _,
+                ))
+                .expect("failed to wait for unpark");
+            };
+            let mut state = self.state.lock().unwrap();
+            if let Some(ready) = state.ready.pop_front() {
+                return ready;
+            } else {
+                unsafe {
+                    ResetEvent(ready_event);
+                }
+            }
+        }
     }
 }
 
 impl Notify for ComUnpark {
     fn notify(&self, id: usize) {
+        let mut state = self.state.lock().unwrap();
+        state.ready.push_back(id);
         unsafe {
-            SetEvent(id as *mut _);
+            SetEvent(state.ready_event);
         }
     }
 
     fn clone_id(&self, id: usize) -> usize {
         let mut state = self.state.lock().unwrap();
-        let pos = state
+        let refs = state
             .handles
-            .binary_search(&id)
+            .get_mut(&id)
             .expect("tried to clone nonexistant id");
-        state.refs[pos] += 1;
+        *refs += 1;
         id
     }
 
     fn drop_id(&self, id: usize) {
         let mut state = self.state.lock().unwrap();
-        let pos = state
+        let refs = state
             .handles
-            .binary_search(&id)
+            .get_mut(&id)
             .expect("tried to drop nonexistant id");
-        let refs = &mut state.refs[pos];
         match *refs {
             1 => {
-                state.refs.remove(pos);
-                let handle = state.handles.remove(pos);
-                unsafe { CloseHandle(handle as *mut _) };
+                state.handles.remove(&id);
             }
             other => {
                 *refs = other - 1;
